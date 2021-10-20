2a737adcc07f46e706d3762eb9dd44554886bd59
Enes Erkol <eneserkol4096@gmail.com>
2021-10-20 15:27:32 +0300

    initial

---
+++ index.js
@@ -0,0 +1,79 @@
+var drums = document.querySelectorAll(".drum").length;
+
+for (var i = 0; i < drums; i++) {
+
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
+
+        var buttonInnerHTML = this.innerHTML;
+        makeSound(buttonInnerHTML);
+        buttonAnimation(buttonInnerHTML);
+
+
+    });
+}
+
+document.addEventListener("keydown", function (event) {
+    makeSound(event.key);
+    buttonAnimation(event.key);
+})
+
+function makeSound(key) {
+
+    switch (key) {
+        case "a":
+            var audio = new Audio('sounds/crash.mp3');
+            audio.play();
+
+            break;
+        case "s":
+            var audio = new Audio('sounds/kick-bass.mp3');
+            audio.play();
+
+            break;
+        case "d":
+            var audio = new Audio('sounds/snare.mp3');
+            audio.play();
+
+            break;
+        case "f":
+            var audio = new Audio('sounds/tom-1.mp3');
+            audio.play();
+
+            break;
+        case "g":
+            var audio = new Audio('sounds/tom-2.mp3');
+            audio.play();
+
+            break;
+        case "h":
+            var audio = new Audio('sounds/tom-3.mp3');
+            audio.play();
+
+            break;
+        case "j":
+            var audio = new Audio('sounds/tom-4.mp3');
+            audio.play();
+
+            break;
+
+        default:
+            break;
+    }
+
+}
+
+function buttonAnimation(currentKey) {
+    var activeButton = document.querySelector("." + currentKey);
+    activeButton.classList.add("pressed");
+
+    setTimeout(function () {
+        activeButton.classList.remove("pressed");
+    }, 100)
+
+}
+
+
+
+
+
+
